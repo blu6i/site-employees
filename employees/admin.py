8a5dd2abc4c9a1dd931603f9baccbf9478a3ad1e
Kostya Sheklein <kosheklein@ya.ru>
2025-09-25 16:53:23 +0300

    Добавил модель

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Employee
 
-# Register your models here.
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'middle_name', 'phone', 'position', 'email', 'salary', 'date_hired')
+    search_fields = ('first_name', 'last_name', 'middle_name', 'position', 'email')
--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField("Имя", max_length=30)
+    last_name = models.CharField("Фамилия", max_length=30)
+    middle_name = models.CharField("Отчество", max_length=30, null=True)
+    phone = models.CharField("Номер телефона", max_length=12)
+    email = models.EmailField("Электронная почта", unique=True)
+    position = models.CharField("Должность", max_length=128)
+    salary = models.DecimalField("Зарплата", max_digits=10, decimal_places=2)
+    date_hired = models.DateField("Дата приема на работу", auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name} {self.last_name or ""} - {self.position}"
